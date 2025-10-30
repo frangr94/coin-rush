2d5bee7059df27fd033ccc111da9f1e4bb273934
frangr94 <fgarciarosales10@gmail.com>
2025-10-30 11:49:58 -0300

    add simple death animation

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,7 +4,10 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
+	Engine.time_scale = 0.5 # slowdown on death
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1 # reset speed
 	get_tree().reload_current_scene()
