f75480bf8fc397e37e70a384d5df5ccff284f80e
frangr94 <fgarciarosales10@gmail.com>
2025-10-31 13:20:34 -0300

    add music and sfx for coin

--- scripts/coin2.gd
+++ scripts/coin2.gd
@@ -1,9 +1,10 @@
 
 extends Area2D
 @onready var game_manager: Node = $"../../../GameManager"
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Collision effect
 func _on_body_entered(_body: Node2D) -> void:
 	game_manager.add_point()
-	queue_free() # Remove the coin from the map
+	animation_player.play("pickup_animation")
+	#queue_free() # Remove the coin from the map
