320d908608ade0adaba10d01094a3ded87a8cff1
frangr94 <fgarciarosales10@gmail.com>
2025-10-31 18:05:17 -0300

    add controller support

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -1,5 +1,10 @@
 extends Control
 
+@onready var level_1: Button = $levels/level_1
+
+func _ready():
+	$levels/level_1.grab_focus()
+
 func _on_level_1_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
 	
