638ecede22198fde4cd59fe22c2182002926530d
frangr94 <fgarciarosales10@gmail.com>
2025-11-02 20:23:09 -0300

    ui fixes, add level 4 base

---
+++ scripts/start_sign4.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+# cambiar
+@onready var time_label: Label = $"../UI/GridContainer/TimeLabel"
+
+
+func _ready() -> void:
+	$Timer.wait_time = 30
+
+
+# Called when the body collides
+func _on_body_entered(_body: Node2D) -> void:
+	timer.start()
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	time_label.text = str(int(timer.time_left)) + "/" + str(int(timer.wait_time))
+
+
+func _on_timer_timeout() -> void:
+	timer.stop()
+	timer.queue_free()
+	get_tree().change_scene_to_file("res://scenes/game_over.tscn")
