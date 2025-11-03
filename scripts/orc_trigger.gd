2d9297e67af2e70d77c8acec8b878a7e32da3534
frangr94 <fgarciarosales10@gmail.com>
2025-11-03 16:29:20 -0300

    add appearing orcs

---
+++ scripts/orc_trigger.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@onready var player: CharacterBody2D = $"../../../Player"
+@onready var animation_player: AnimationPlayer = $"../orc/AnimationPlayer"
+@onready var orc: Node2D = $"../orc"
+
+
+
+
+func _on_body_entered(body: CharacterBody2D) -> void:
+	animation_player.play("move")
+	$CollisionShape2D.set_deferred("disabled", true)
+	await animation_player.animation_finished
+	orc.queue_free()
+	
+
+		
+	pass # Replace with function body.
---
+++ scripts/orc_trigger2.gd
@@ -0,0 +1,15 @@
+extends Area2D
+@onready var player: CharacterBody2D = $"../../../Player"
+@onready var orc: Node2D = $"../orc"
+
+@onready var animation_player: AnimationPlayer = $"../orc/AnimationPlayer"
+
+
+
+func _on_body_entered(body: CharacterBody2D) -> void:
+	animation_player.play("move")
+	$CollisionShape2D.set_deferred("disabled", true)
+	await animation_player.animation_finished
+	orc.queue_free()
+
+	pass # Replace with function body.
