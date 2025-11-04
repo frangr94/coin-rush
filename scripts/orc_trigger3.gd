d2b6836a851f08e073ed9a0fde726f38a40cd08e
frangr94 <fgarciarosales10@gmail.com>
2025-11-04 13:47:41 -0300

    orcs now work, some ui changes

--- scripts/orc_trigger3.gd
+++ scripts/orc_trigger3.gd
@@ -1,12 +1,11 @@
 extends Area2D
-@onready var player: CharacterBody2D = $"../../../Player"
 @onready var orc: Node2D = $"../orc"
-
-
 @onready var animation_player: AnimationPlayer = $"../orc/AnimationPlayer"
-   
-func _on_body_entered(body: CharacterBody2D) -> void:
+@onready var player: CharacterBody2D = $"../../../Player"
+
+
+
+func _on_body_entered(body: Node2D) -> void:
 	animation_player.play("move")
 	$CollisionShape2D.set_deferred("disabled", true)
 	await animation_player.animation_finished
-	orc.queue_free()
