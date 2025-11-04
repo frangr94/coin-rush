d3a05a91d0091430d799aadc73e92d0e1e431532
frangr94 <fgarciarosales10@gmail.com>
2025-11-04 15:57:13 -0300

    add some progress

---
+++ scripts/orc_trigger4.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+
+@onready var player: CharacterBody2D = $"../../../Player"
+@onready var animation_player: AnimationPlayer = $"../orc/AnimationPlayer"
+@onready var orc: Node2D = $"../orc"
+
+
+func _on_body_entered(body: Node2D) -> void:
+	animation_player.play("move")
+	$CollisionShape2D.set_deferred("disabled", true)
+	await animation_player.animation_finished
